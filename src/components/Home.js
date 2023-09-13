f2c4b66e17c5ae82cd202d24f992b38343c5e909
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-13 19:40:07 +0530

    added Home component

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../styles/Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='home__container'>
+            <img
+            className='home__image'
+            src='https://images-eu.ssl-images-amazon.com/images/G/31/prime/S2S/Homepage_DesktopHeroTemplate_1500x600._CB587504363_.jpg'
+            alt=''/>
+        </div>
+
+        <div className='home__row'>
+            {/* Product */}
+            {/* Product */}
+            {/* Product */}
+        </div>
+
+        <div className='home__row'>
+            {/* Product */}
+            {/* Product */}
+            {/* Product */}
+            {/* Product */}
+            {/* Product */}
+        </div>
+
+        <div className='home__row'>
+            {/* Product */}
+            {/* Product */}
+        </div>
+    </div>
+  )
+}
+
+export default Home
