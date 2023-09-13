fb43aa27efd8a812a2149091db7eafe958ea5f03
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-13 13:34:52 +0530

    first commit Header

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD5XsIP8PZrruvJtXvb8K3BxOVxyXvuvi0",
+    authDomain: "clone-fae3f.firebaseapp.com",
+    projectId: "clone-fae3f",
+    storageBucket: "clone-fae3f.appspot.com",
+    messagingSenderId: "421016223451",
+    appId: "1:421016223451:web:b48a421a872054c97356fe",
+    measurementId: "G-9KPHRREFD6"
+  };
