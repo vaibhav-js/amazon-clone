176158deb8069504cb87efde6908791c85f7fddf
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-14 14:32:35 +0530

    added 'Link to' functionality to amazon logo and cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,19 @@ import React from "react"
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import "../styles/Header.css"
+import { Link } from "react-router-dom";
 
 
 function Header() {
   return (
     <div className="header">
-        <img
-        className="header__logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="logo"
-        />
+        <Link to='/'>
+            <img
+                className="header__logo"
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="logo"
+            />
+        </Link>
 
         <div className="header__search">
             <input
@@ -37,10 +40,12 @@ function Header() {
                 <span className="header__optionLineTwo">Prime</span>
             </div>
 
-            <div className="header__optionCart">
-                <ShoppingCartIcon />
-                <span className="header__optionLineTwo header__cartCount">0</span>
-            </div>
+            <Link to='/checkout'>
+                <div className="header__optionCart">
+                    <ShoppingCartIcon />
+                    <span className="header__optionLineTwo header__cartCount">0</span>
+                </div>
+            </Link>
         </div>
     </div>
   )
