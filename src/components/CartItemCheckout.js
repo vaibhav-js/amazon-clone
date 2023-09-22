bf54e3296ecdfbefdf2c6e5f05af3d69a57a633a
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-22 20:04:19 +0530

    added CartItemCheckout component

---
+++ src/components/CartItemCheckout.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../styles/cartItemCheckout.css'
+import { useStateValue } from './StateProvider'
+
+
+function CartItemCheckout({ id, image, title, price, rating}) {
+    const [{ cart }, dispatch] = useStateValue();
+
+    const removeFromCart = () => {
+
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            item: {
+                id,
+                image,
+                title,
+                price,
+                rating
+            }
+        })
+    }
+
+  return (
+    <div className='cartItemCheckout'>
+        <img
+            src={image}
+            alt='product'
+        />
+
+        <div className='cartItemCheckout__info' key={id}>
+            <p className='cartItemCheckout__title'>{title}</p>
+            <p className='cartItemCheckout__price'>
+                <small><sup>₹</sup></small>
+                <strong>{price}</strong>
+            </p>
+
+            <div className='cartItemCheckout__rating'>
+                {Array(rating).fill().map((_, i) => (
+                    <p key={i}>⭐</p>
+                ))}
+            </div>
+            <button onClick={removeFromCart}>Remove from Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default CartItemCheckout
