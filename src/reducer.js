e45e9c93dade7daa9bc5ccf3e8acc3078e51606c
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-22 20:06:20 +0530

    added REMOVE_FROM_CART functionality

--- src/reducer.js
+++ src/reducer.js
@@ -5,12 +5,27 @@ export const initialState = {
 export const getCartTotal = (cart) => cart?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
-    console.log(action)
     switch(action.type) {
         case "ADD_TO_CART":
             return {
                 ...state,
                 cart: [...state.cart, action.item]
+            };
+
+        case "REMOVE_FROM_CART":
+            const index = state.cart.findIndex((cartItem) => cartItem.id === action.item.id);
+            let newCart = [...state.cart];
+
+            if (index >= 0) {
+                newCart.splice(index, 1);
+            } else {
+                console.warn(
+                    `Can't remove product (id: ${action.id}) as it is not in cart!`
+                )
+            }
+            return {
+                ...state,
+                cart: newCart
             }
         default:
             return state;
