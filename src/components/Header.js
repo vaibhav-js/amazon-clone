08cc3a8416474f732e5462051c351d12cdfd4477
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-25 17:29:09 +0530

    showing user email on header after authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,9 +36,9 @@ function Header() {
         </div>
 
         <div className="header__nav">
-            <Link to='/login'>
+            <Link to={!user && '/login'}>
                 <div onClick={handleAuthentication} className="header__option">
-                    <span className="header__optionLineOne">Hello, </span>
+                    <span className="header__optionLineOne">Hello, {user ? user.email : "Guest"} </span>
                     <span className="header__optionLineTwo">{user ? "Sign out": "Sign in"}</span>
                 </div>
             </Link>
