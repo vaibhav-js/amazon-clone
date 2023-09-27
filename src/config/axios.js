1eaaf69022b70f4fd7c32164b13ea7e17eb87332
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-27 21:50:06 +0530

    changed localhost-cloud-function-url to firebase-cloud-function-url

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
      // THE API (cloud function) URL
-    baseURL: `http://localhost:5001/clone-fae3f/us-central1/api`
+    baseURL: "https://us-central1-clone-fae3f.cloudfunctions.net/api"
+              // "http://localhost:5001/clone-fae3f/us-central1/api"
 })
 
 export default instance;
