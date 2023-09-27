b9ce6e2b0d5ea87d0eae28d4e17143f42384d7a8
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-27 17:37:44 +0530

    added Orders component

---
+++ src/components/Orders.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/Orders.css"
+
+function Orders() {
+  return (
+    <div className='orders'>
+        Order Placed
+    </div>
+  )
+}
+
+export default Orders
