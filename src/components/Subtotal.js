179c1049721d5eceb632abf4df9c14561c7ac480
vaibhav-js <vaibhavrajpianist@gmail.com>
2023-09-28 11:31:59 +0530

    added button to disable when navigating to payment

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -4,13 +4,19 @@ import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from './StateProvider'
 import { getCartTotalAmount, getCartTotalItems } from '../reducer'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 function Subtotal() {
     const [{ cart }] = useStateValue();
+    const [disabled, setDisabled] = useState(false)
     const navigate = useNavigate();
 
+
     const navigateToPayment = () => {
-        navigate('/payment')
+        if (cart?.length) {
+            setDisabled(true);
+            navigate('/payment')
+        }
     }
 
   return (
@@ -33,7 +39,7 @@ function Subtotal() {
             thousandSeparator={true}
             prefix='₹'
         />
-        <button onClick={navigateToPayment}>Proceed to Buy</button>
+        <button onClick={navigateToPayment} disabled={disabled}>Proceed to Buy</button>
     </div>
   )
 }
